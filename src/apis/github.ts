c3255b20335aba966b5d7cec7e242a0e37548d5b
owen970517 <tamia123123@naver.com>
2023-09-02 14:26:56 +0900

    refactor: axios interceptor 추가

--- src/apis/github.ts
+++ src/apis/github.ts
@@ -2,17 +2,30 @@ import axios from "axios";
 import { API_URL } from "../constants/urls";
 const baseURL = `${API_URL.github}/${API_URL.organization}/${API_URL.repo}`
 interface paramsProps {
-    state: string;
-    sort : string;
-    page : number;
+  state: string;
+  sort : string;
+  page : number;
 }
 
 const githubAxios = axios.create({
   baseURL,
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_GIT_TOKEN}`,
+    Authorization: `Bearer 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`,
   },
 });
+
+githubAxios.interceptors.request.use(
+  config => {
+      const GIT_TOKEN = process.env.REACT_APP_GIT_TOKEN;
+
+      if (GIT_TOKEN) config.headers.Authorization = `Bearer ${GIT_TOKEN}`;
+      return config;
+  },
+  error => {
+      return error;
+  }
+);
+
 
 export const getIssues = async (params:paramsProps) => {
     const data = await githubAxios.get('/issues', {
